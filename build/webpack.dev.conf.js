7f432b4e2d3aecd251fa37d907e4b1a6c9457bfc
tangyy <yueyangtang@163.com>
2019-05-05 14:40:08 +0800

    Add 注释

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,6 +1,7 @@
 const config = require('./webpack.base.conf')
 module.exports = {
   ...config,
+  // 会将 process.env.NODE_ENV 的值设为 development。启用 NamedChunksPlugin 和 NamedModulesPlugin。
   mode: 'development',
   devServer: {
     contentBase: './dist',
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,6 +1,7 @@
 const config = require('./webpack.base.conf')
 module.exports = {
   ...config,
+  // 会将 process.env.NODE_ENV 的值设为 production。启用 FlagDependencyUsagePlugin, FlagIncludedChunksPlugin, ModuleConcatenationPlugin, NoEmitOnErrorsPlugin, OccurrenceOrderPlugin, SideEffectsFlagPlugin 和 UglifyJsPlugin.
   mode: 'production',
   devServer: {
     contentBase: './dist',
